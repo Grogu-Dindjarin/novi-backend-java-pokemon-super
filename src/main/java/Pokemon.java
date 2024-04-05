8b4bc88dbd92f5aabc6cf14d407749a735a8d506
Grogu-Dindjarin <grogu.dindjarin9@gmail.com>
2024-04-06 00:15:40 +0200

    Create Pokemon abstract super class

--- src/main/java/Pokemon.java
+++ src/main/java/Pokemon.java
@@ -1,120 +1,51 @@
 package main.java;
 
-public class Pokemon {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class Pokemon {
     private String name;
     private int level;
     private int hp;
-    private int xp;
-    private double height;
-    private double weight;
-    private String food;
-    private String sound;
+    private int baseExp;
     private int defence;
     private int attack;
-    private String special;
+    private String food;
+    private String sound;
+    private int special;
+    private double accuracy;
+    private String speech;
 
-    public String getName() {
-        return name;
+    public Pokemon() {
     }
 
-    public void setName(String name) {
+    public Pokemon(String name,
+                   int level,
+                   int hp,
+                   int baseExp,
+                   int defence,
+                   int attack,
+                   String food,
+                   String sound,
+                   int special,
+                   double accuracy) {
         this.name = name;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
         this.level = level;
-    }
-
-    public int getHp() {
-        return hp;
-    }
-
-    public void setHp(int hp) {
         this.hp = hp;
-    }
-
-    public int getXp() {
-        return xp;
-    }
-
-    public void setXp(int xp) {
-        this.xp = xp;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public String getFood() {
-        return food;
-    }
-
-    public void setFood(String food) {
-        this.food = food;
-    }
-
-    public String getSound() {
-        return sound;
-    }
-
-    public void setSound(String sound) {
-        this.sound = sound;
-    }
-
-    public int getDefence() {
-        return defence;
-    }
-
-    public void setDefence(int defence) {
+        this.baseExp = baseExp;
         this.defence = defence;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    public void setAttack(int attack) {
         this.attack = attack;
-    }
-
-    public String getSpecial() {
-        return special;
-    }
-
-    public void setSpecial(String special) {
+        this.food = food;
+        this.sound = sound;
         this.special = special;
-    }
-
-    public double getAccuracy() {
-        return accuracy;
-    }
-
-    public void setAccuracy(double accuracy) {
         this.accuracy = accuracy;
     }
 
-    private double accuracy;
-
-    public Pokemon(String name, int level, int hp, int xp) {
-        this.name = name;
-        this.level = level;
-        this.hp = hp;
-        this.xp = xp;
+    public void eats() {
+        System.out.println(this.getName() + " is eating " + this.getFood() + " sounds like " + this.getName() + " is really enjoying this! " + this.getSound() + "\n");
     }
+
+    public abstract void speak(String speech);
 }
